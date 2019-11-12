267797831fa156eb0a49a8b0a8c0ebc5fbf50713
KimJungHwan96 <wjdghks3377@ajou.ac.kr>
2019-11-12 14:18:22 +0900

    sibal

---
+++ models/chatRoom.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const ChatRoomSchema = mongoose.Schema({
+    name: { type: String }, // 이거 없어도 되는거 아니냐
+    interests: [{ type: String }],
+    participants: [{
+        email: { type: String, required: true, unique: true },
+        nickname: { type: String},
+        interests: [{ type: String }]
+    }]
+});
+
+ChatRoomSchema.statics.findRoomByKeyword = function(keyword) {
+    return this.find({interests: keyword});
+};
+
+module.exports = mongoose.model('chatroom', ChatRoomSchema);
